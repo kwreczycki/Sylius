9985e10f9c841373b7100b6f33b695014d3487e3
Christian Daguerre <christian.daguerre@gmail.com>
2015-04-01 18:01:09 +0200

    add channel and currency information to sylius data collector

--- src/Sylius/Bundle/CoreBundle/DataCollector/SyliusDataCollector.php
+++ src/Sylius/Bundle/CoreBundle/DataCollector/SyliusDataCollector.php
@@ -15,15 +15,34 @@ use Sylius\Bundle\CoreBundle\Kernel\Kernel;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+use Sylius\Component\Channel\Context\ChannelContextInterface;
+use Sylius\Component\Currency\Context\CurrencyContextInterface;
 
 class SyliusDataCollector extends DataCollector
 {
+    protected $currencyContext;
+    protected $channelContext;
+
+    /**
+     * Constructor.
+     *
+     * @param CurrencyContextInterface $currencyContext
+     * @param ChannelContextInterface  $channelContext
+     */
+    public function __construct(CurrencyContextInterface $currencyContext, ChannelContextInterface $channelContext)
+    {
+        $this->currencyContext = $currencyContext;
+        $this->channelContext  = $channelContext;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function collect(Request $request, Response $response, \Exception $exception = null)
     {
         $this->data['version'] = Kernel::VERSION;
+        $this->data['currency'] = $this->currencyContext->getCurrency();
+        $this->data['channel'] = $this->channelContext->getChannel();
     }
 
     /**
@@ -32,6 +51,22 @@ class SyliusDataCollector extends DataCollector
     public function getVersion()
     {
         return $this->data['version'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrency()
+    {
+        return $this->data['currency'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getChannel()
+    {
+        return $this->data['channel'];
     }
 
     /**
